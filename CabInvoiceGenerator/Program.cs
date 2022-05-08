a9dde3c59e79e2a153aa95391238b52331adbab9
Arun37-Kumar <arun007624@gmail.com>
2022-05-08 10:31:10 +0530

    UC2 updated

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -7,6 +7,9 @@ namespace CabInvoiceGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Cab Invoice Generator!");
+            CabInvoiceGenerater fare = new CabInvoiceGenerater(RideType.NORMAL_RIDE);
+            double result = fare.CalculateFare(10,20);
+            Console.WriteLine("Fare is : " + result);
             Console.ReadLine();
         }
     }
